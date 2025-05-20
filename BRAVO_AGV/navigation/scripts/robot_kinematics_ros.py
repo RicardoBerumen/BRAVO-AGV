970d67b7937478d483c74a0acba7e2fbd875a1e7
RicardoBerumen <A01641180@tec.mx>
2025-05-19 19:30:49 -0600

    creation of ROS node for robot control

--- BRAVO_AGV/navigation/scripts/robot_kinematics_ros.py
+++ BRAVO_AGV/navigation/scripts/robot_kinematics_ros.py
@@ -5,7 +5,7 @@ from time import sleep
 import rclpy
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Twist
 
 class robot_control(Node):
     """
@@ -13,58 +13,92 @@ class robot_control(Node):
     """
     def __init__(self):
         super().__init__("robot_control")
+        self.uart_config()
+        self.vel_subscription = self.create_subscription(
+            Twist,
+            'cmd_vel',
+            self.vel_callback,
+            10
+        )
+        self.vel_subscription
+        """
+        Robot Kinematics Definition
+        """
+        
+
+        self.l = 0.5
+        self.w_r = 0.1016
+    
+    def uart_config(self):
+        """
+        Config section
+        """
+        self.ser = serial.Serial("/dev/ttyAMA0", 115200)
+        ser = self.ser
+        print("Configuration ready")
+        print(ser.name)
+        self.newline = "\n"
+
+    def vel_callback(self, msg):
+        self.get_logger().info("I heard: %d", %msg.linear.x)
+        self.V = msg.linear.x
+        self.w = msg.angular.z
+
+        V = self.V
+        w = self.w
+        l = self.l
+        w_r = self.w_r
+
+        vr = (2*V + l*w)/2
+        vl = (2*V - l*w)/2
+
+        vr_rpm = rpm(vr, w_r)
+        vl_rpm = rpm(vl, w_r)
+
+        self.vr_rpm = vr_rpm
+        self.vl_rpm = vl_rpm
+
+        """
+        UART Encoding and communication
+        """
+        # Encoding values
+        vrn = str(vr_rpm)
+        vln = str(vl_rpm)
+        print(vrn)
+        print(vln)
+
+        i = 0
+        ser = self.ser 
+        newline = self.newline
+        try:
+            ser.write(b'1 \n')
+            ser.write((vrn.encode()+newline.encode()))
+            ser.write(b'2 \n')
+            ser.write((vln.encode()+newline.encode()))
+            sleep(0.01)
+
+        except KeyboardInterrupt:
+            print("closing")
+        ser.close()
+
 
 
 def rpm(ms, w):
     v_rpm = ms/w*2*pi/60
     return round(v_rpm, 6)
 
-"""
-Config section
-"""
-ser = serial.Serial("/dev/ttyAMA0", 115200)
-print("Configuration ready")
-print(ser.name)
-newline = "\n"
+def main(args = None):
 
-"""
-Robot Kinematics Definition
-"""
+    """
+    ROS Node
+    """
+    rclpy.init(args=args)
+    velocity_subscriber = robot_control()
+    rclpy.spin(velocity_subscriber)
+    
+    # destroy node
+    velocity_subscriber.destroy_node()
+    rclpy.shutdown()
 
-V = 1.1
-w = 1.1
-
-l = 0.5
-w_r = 0.1016
-
-vr = (2*V + l*w)/2
-vl = (2*V - l*w)/2
-
-vr_rpm = rpm(vr, w_r)
-vl_rpm = rpm(vl, w_r)
-
-
-"""
-UART Encoding and communication
-"""
-# Encoding values
-vrn = str(vr_rpm)
-vln = str(vl_rpm)
-print(vrn)
-print(vln)
-
-i = 0
-
-while True:
-    try:
-        ser.write(b'1 \n')
-        ser.write((vrn.encode()+newline.encode()))
-        ser.write(b'2 \n')
-        ser.write((vln.encode()+newline.encode()))
-        sleep(0.01)
-
-    except KeyboardInterrupt:
-        break
-
-print("closing")
-ser.close()
+if __name__ == '__main__':
+    main()
