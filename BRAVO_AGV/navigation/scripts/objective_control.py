344def0c39a34dd2a4bdb0d7a25c96cf4c062127
RicardoBerumen <ricberar@gmail.com>
2025-06-04 20:17:39 -0600

    Control tunning

--- BRAVO_AGV/navigation/scripts/objective_control.py
+++ BRAVO_AGV/navigation/scripts/objective_control.py
@@ -29,17 +29,19 @@ class obj_control(Node):
         """
         self.l = 0.63
         self.w_r = 0.1016
-        self.kpr = 1
-        self.kpt = 1
-        self.kgamma = 1
-        self.v_max = 3
+        self.kpr = 1.0
+        self.kpt = 1.0
+        self.kgamma = 1.0
+        self.v_max = 3.0
 
-        self.x = 0
-        self.y = 0
-        self.theta = 0
-        self.z_d = 0
-        self.x_d = 0
-        self.y_d = 0
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
+        self.z_d = 0.0
+        self.x_d = 0.0
+        self.y_d = 0.0
+        self.x_df = 0.0
+        self.y_df = 0.0
         self.get_clock().now()
         self.tiempo = self.get_clock().now().nanoseconds/1000000000
         self.tiempo_ant = self.tiempo
@@ -49,24 +51,28 @@ class obj_control(Node):
             self.z_d = msg.point.z
             self.x_d = msg.point.x
             self.y_d = msg.point.y
-            x_df = self.x + self.z_d*1000 - self.l
-            y_df = self.y + self.x_d*1000 - self.l
-        self.get_logger().info('Z distance: "{0}"'.format(msg.point.z))
-        self.get_logger().info('X distance: "{0}"'.format(msg.point.x))
-        self.get_logger().info('Y distance: "{0}"'.format(msg.point.y))
+            self.x_df = self.x + self.z_d/1000.0
+            self.y_df = self.y + self.x_d/1000.0
+        self.get_logger().info('Z distance: "{0}"'.format(msg.point.z/1000))
+        self.get_logger().info('X distance: "{0}"'.format(msg.point.x/1000))
+        self.get_logger().info('Y distance: "{0}"'.format(msg.point.y/1000))
         self.tiempo = self.get_clock().now().nanoseconds/1000000000
+        x_df = self.x_df
+        y_df = self.y_df
 
         try:
             thetad = atan2((y_df-self.y), (x_df-self.x))
             thetae = self.theta - thetad
 
             self.d = x_df - self.x
+            self.get_logger().info('Position X: "{0}"'.format(self.x))
+            self.get_logger().info('Distance to target: "{0}"'.format(self.d))
             self.gamma = -tanh((self.kgamma)*abs(thetae))+1
 
             if self.d > self.l:
                 self.V = self.gamma*self.v_max*tanh((self.kpt*self.d**2)/self.v_max)
             else:
-                self.V = 0
+                self.V = 0.0
 
             twist = Twist()
             twist.linear.x = self.V
@@ -79,6 +85,7 @@ class obj_control(Node):
             self.vel_pub.publish(twist)
 
             self.dt = self.tiempo - self.tiempo_ant
+            self.tiempo_ant = self.tiempo
             self.x = self.x + self.V*self.dt
 
 
